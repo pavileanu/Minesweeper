0d9d4c844be11a07489a96bc99e4dda9685e4d29
pavimircea <mirceashark2007@yahoo.com>
2020-04-05 00:03:34 +0300

    All Feauture Completed, Minesweeper full version

--- matrix.js
+++ matrix.js
@@ -40,6 +40,9 @@ function createUITable(){
 
 function cellMatrixClicked(ev, x, y){
 
+  if( x == null && y == null)
+    ev.stopPropagation();
+
   var currentCellValue;
   if(x == null )
     x=ev.target.getAttribute("prim");
@@ -63,20 +66,27 @@ function cellMatrixClicked(ev, x, y){
 
     if(x > 0 && y > 0)
       cellMatrixClicked(ev, Number(x-1), Number(y-1));
-    if(x < 9 && y < 9)
-      cellMatrixClicked(ev, Number(x)+1, Number(y)+1);
-    if(x > 0 && y < 9)
-      cellMatrixClicked(ev, Number(x-1), Number(y+1));
-    if(x < 9 && y > 0)
-      cellMatrixClicked(ev, Number(x)+1, Number(y-1));
+
     if(x > 0)
       cellMatrixClicked(ev, Number(x-1), Number(y));
+
+    if(x > 0 && y < 9)
+      cellMatrixClicked(ev, Number(x-1), Number(y)+1);
+
     if(y > 0)
       cellMatrixClicked(ev, Number(x), Number(y-1));
-    if(x < 9)
-      cellMatrixClicked(ev, Number(x)+1, Number(y));
+
     if(y < 9)
       cellMatrixClicked(ev, Number(x), Number(y)+1);
+    
+    if(x < 9 && y > 0)
+      cellMatrixClicked(ev, Number(x)+1, Number(y-1));
+
+    if(x < 9)
+      cellMatrixClicked(ev, Number(x)+1, Number(y));
+
+    if(x < 9 && y < 9)
+      cellMatrixClicked(ev, Number(x)+1, Number(y)+1);
   }
 
   winTest();
@@ -88,30 +98,7 @@ function createDataTable(mineTable)
   for(x=0;x<10;x++){
     matrixTable[x] = new Array(10);
     for(y=0;y<10;y++)
-      if(mineTable[x][y]==0){
-        if(x==0&&y==0)
-          matrixTable[x][y]=mineTable[0][1]+mineTable[1][1]+mineTable[1][0];  
-        if(x==0&&y==9)
-          matrixTable[x][y]=mineTable[0][8]+mineTable[1][8]+mineTable[1][9];
-        if(x==9&&y==0)
-          matrixTable[x][y]=mineTable[8][0]+mineTable[9][1]+mineTable[8][1];
-        if(x==9&&y==9)
-          matrixTable[x][y]=mineTable[8][8]+mineTable[8][9]+mineTable[9][8];
-          //colturi final
-  
-        if(y==0&&x!=0&&x!=9)
-          matrixTable[x][y]=mineTable[x+1][y]+mineTable[x-1][y]+mineTable[x-1][y+1]+mineTable[x][y+1]+mineTable[x+1][y+1];
-        if(x==0&&y!=0&&y!=9)
-          matrixTable[x][y]=mineTable[x][y-1]+mineTable[x][y+1]+mineTable[x+1][y-1]+mineTable[x+1][y+1]+mineTable[x+1][y];
-        if(x==9&&y!=0&&y!=9)
-          matrixTable[x][y]=mineTable[x][y-1]+mineTable[x][y+1]+mineTable[x-1][y-1]+mineTable[x-1][y]+mineTable[x-1][y+1];
-        if(y==9&&x!=0&&x!=9)
-          matrixTable[x][y]=mineTable[x-1][y]+mineTable[x+1][y]+mineTable[x-1][y-1]+mineTable[x][y-1]+mineTable[x+1][y-1];
-          //margini final
-        if(x!=0&&y!=0&&x!=9&&y!=9)
-          matrixTable[x][y]=mineTable[x-1][y-1]+mineTable[x-1][y]+mineTable[x-1][y+1]+mineTable[x][y-1]+mineTable[x][y+1]+mineTable[x+1][y-1]+mineTable[x+1][y]+mineTable[x+1][y+1];
-      }
-      else matrixTable[x][y]=-1;
+      matrixTable[x][y] = calculateCellValue(x, y);
     }
 }
 
@@ -139,8 +126,10 @@ function addRightClickEvenCell(casuta)
 
 function FlaggedMinesArroundCell(i, j){
  
+  i = Number(i);
+  j = Number(j); 
   var flaggedMinesSelected = [];
-  for(k = 0; k < flaggedMines.length; k++){
+  for(k = 0; k < flaggedMines.length; k++)
     if((flaggedMines[k].x == i-1 && flaggedMines[k].y == j-1) ||
       (flaggedMines[k].x == i-1 && flaggedMines[k].y == j)  ||
       (flaggedMines[k].x == i-1 && flaggedMines[k].y == j+1) ||
@@ -151,28 +140,32 @@ function FlaggedMinesArroundCell(i, j){
       (flaggedMines[k].x == i+1 && flaggedMines[k].y == j+1))
       flaggedMinesSelected.push(new mine(flaggedMines[k].x, flaggedMines[k].y));
   return flaggedMinesSelected;
-  }
+  
 }
 
 function indicatorPressed(ev)
 {
   var x=ev.target.getAttribute("prim");
   var y=ev.target.getAttribute("doi");
+
+  if(isFlaggedMine(x, y))
+    return;
+
   var fm = FlaggedMinesArroundCell(x, y);
   if (fm == null) fm = [];
   var checkCount = 0;
   if(fm.length > 0)
   {
+
     for(var k = 0; k < fm.length; k++)
       if(matrixTable[fm[k].x][fm[k].y] == -1)
         checkCount++;
 
-    if(checkCount == fm.length){
-      showCells(x, y);
-    }
-    else{
-      loose();
-    }
+    if(fm.length == calculateCellValue(x, y))
+      if( checkCount == fm.length)      
+        showCells(x, y);
+      else
+        loose(); 
 
   }
   hooverCells(x, y);
@@ -180,51 +173,41 @@ function indicatorPressed(ev)
 
 
 function hooverCells(i, j){
-//Number(
-  if(matrixTable[i-1][j-1] != 7){
-    hooverCell(i-1, j-1);
-  }
 
-  //try{
-  if(matrixTable[i-1][j] != 7)
-    hooverCell(i-1, j);
+  i = Number(i);
+  j = Number(j);
+    
+  if(i > 0 && j > 0)
+    if( matrixTable[i-1][j-1] != 7)
+      hooverCell(i-1, j-1);
+
+  if(i > 0)
+    if( matrixTable[i-1][j] != 7)
+      hooverCell(i-1, j);
+    
+  if(i > 0 && j < 9)
+    if(matrixTable[i-1][j+1] != 7)
+      hooverCell(i-1, j+1);
+
+  if(j > 0)
+    if(matrixTable[i][j-1] != 7)
+      hooverCell(i, j-1);
   
-  //}catch{}
+  if(j < 9)
+    if(matrixTable[i][j+1] != 7)
+      hooverCell(i, j+1);
 
-  //try{
-    if(matrixTable[Number(i)-1][Number(j)+1] != 7)
-      hooverCell(Number(i)-1, Number(j)+1);
-  //}
-  //catch{}
+  if(i < 9 && j > 0)
+    if( matrixTable[i+1][j-1] != 7)
+      hooverCell(i+1, j-1);
 
-  //try{
-  if(matrixTable[i][j-1] != 7)
-    hooverCell(i, j-1);
-  //}catch{}
+  if(i < 9)
+    if( matrixTable[i+1][j] != 7)
+      hooverCell(i+1, j);
 
-  //try{
-  if(matrixTable[i][Number(j)+1] != 7)
-    hooverCell(i, Number(j)+1);
-  
-  //}catch{}
-
-  //try{
-    if(matrixTable[Number(i)+1][j-1] != 7)
-      hooverCell(Number(i)+1, j-1);
-  //}
-  //catch{}
-
-  //try{
-    if(matrixTable[Number(i)+1][j] != 7)
-      hooverCell(Number(i)+1, j);
-  //}
-  //catch{}
-
-  //try{
-    if(matrixTable[Number(i)+1][Number(j)+1] != 7)
-      hooverCell(Number(i)+1, Number(j)+1);
-  //}
-  //catch{}
+  if(i < 9 && j < 9)
+    if( matrixTable[i+1][j+1] != 7)
+      hooverCell(i+1, j+1);
 
 }
 
@@ -236,56 +219,43 @@ function loose(){
   gameMatrix.querySelectorAll('button').forEach(function(button){button.setAttribute("disabled", "true");});
 }
 
-
 function showCells(i, j){
-  try{
-    if(martixTable[i-1][j-1] != -1)
-      showCell(i-1, j-1);
-  }
-  catch{
-    return;
-  }
 
-  try{
-    if(matrixTable[i-1][j] != -1)
-      showCell(i-1, j);
-    }
-  catch{}
+  i = Number(i);
+  j = Number(j);
+    
+  if(i > 0 && j > 0)
+    if(matrixTable[i-1][j-1] != -1 &&  matrixTable[i-1][j-1] != 7)
+      cellMatrixClicked(null, i-1, j-1);
 
-  try{
-    if(matrixTable[i-1][j+1] != -1)
-      showCell(i-1, j+1);
-  }
-  catch{}
+  if(i > 0)
+    if(matrixTable[i-1][j] != -1 &&  matrixTable[i-1][j] != 7)
+      cellMatrixClicked(null, i-1, j);
+    
+  if(i > 0 && j < 9)
+    if(matrixTable[i-1][j+1] != -1 && matrixTable[i-1][j+1] != 7)
+      cellMatrixClicked(null, i-1, j+1);
 
-  try{
-  if(matrixTable[i][j-1] != -1)
-    showCell(i, j-1);}
-  catch{}
+  if(j > 0)
+    if(matrixTable[i][j-1] != -1 && matrixTable[i][j-1] != 7)
+      cellMatrixClicked(null, i, j-1);
+  
+  if(j < 9)
+    if(matrixTable[i][j+1] != -1 && matrixTable[i][j+1] != 7)
+      cellMatrixClicked(null, i, j+1);
 
-  try{
-  if(matrixTable[i][j+1] != -1)
-    showCell(i, j+1);
-  }
-  catch{}
+  if(i < 9 && j > 0)
+    if(matrixTable[i+1][j-1] != -1 &&  matrixTable[i+1][j-1] != 7)
+      cellMatrixClicked(null, i+1, j-1);
 
-  try{
-    if(matrixTable[i+1][j-1] != -1)
-      showCell(i+1, j-1);
-  }
-  catch{}
+  if(i < 9)
+    if(matrixTable[i+1][j] != -1 &&  matrixTable[i+1][j] != 7)
+      cellMatrixClicked(null, i+1, j);
 
-  try{
-    if(matrixTable[i+1][j] != -1)
-      showCell(i+1, j);
-  }
-  catch{}
+  if(i < 9 && j < 9)
+    if(matrixTable[i+1][j+1] != -1 && matrixTable[i+1][j+1] != 7)
+      cellMatrixClicked(null, i+1, j+1);
 
-  try{
-    if(matrixTable[i+1][j+1] != -1)
-      showCell(i+1, j+1);
-  }
-  catch{}
 }
 
 function hooverCell(i, j){
@@ -308,6 +278,42 @@ function showCell(i, j){
   matrixTableUI.rows[i].cells[j].innerHTML=" "+matrixTable[i][j]+" ";  
   matrixTable[i][j]=7;
 } 
+
+function calculateCellValue(x, y){
+  var cellValue;
+
+  x = Number(x);
+  y= Number(y);
+
+  if(mineTable[x][y]==0){
+    if(x==0&&y==0)
+      cellValue = mineTable[0][1]+mineTable[1][1]+mineTable[1][0];  
+    if(x==0&&y==9)
+      cellValue =mineTable[0][8]+mineTable[1][8]+mineTable[1][9];
+    if(x==9&&y==0)
+      cellValue =mineTable[8][0]+mineTable[9][1]+mineTable[8][1];
+    if(x==9&&y==9)
+      cellValue=mineTable[8][8]+mineTable[8][9]+mineTable[9][8];
+          //colturi final
+  
+    if(y==0&&x!=0&&x!=9)
+      cellValue=mineTable[x+1][y]+mineTable[x-1][y]+mineTable[x-1][y+1]+mineTable[x][y+1]+mineTable[x+1][y+1];
+    if(x==0&&y!=0&&y!=9)
+      cellValue=mineTable[x][y-1]+mineTable[x][y+1]+mineTable[x+1][y-1]+mineTable[x+1][y+1]+mineTable[x+1][y];
+    if(x==9&&y!=0&&y!=9)
+      cellValue=mineTable[x][y-1]+mineTable[x][y+1]+mineTable[x-1][y-1]+mineTable[x-1][y]+mineTable[x-1][y+1];
+    if(y==9&&x!=0&&x!=9)
+      cellValue=mineTable[x-1][y]+mineTable[x+1][y]+mineTable[x-1][y-1]+mineTable[x][y-1]+mineTable[x+1][y-1];
+      //margini final
+    if(x!=0&&y!=0&&x!=9&&y!=9)
+      cellValue=mineTable[x-1][y-1]+mineTable[x-1][y]+mineTable[x-1][y+1]+mineTable[x][y-1]+mineTable[x][y+1]+mineTable[x+1][y-1]+mineTable[x+1][y]+mineTable[x+1][y+1];
+  
+  }
+  else cellValue = -1;
+
+  return cellValue;
+
+}
 
 
 
